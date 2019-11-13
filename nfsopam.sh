3330b6fca341ca54352d53cb81bb6f77e150a37d
Aleksandar Popadic <saso@gauss.cmm.ki.si>
2019-11-13 16:28:34 +0100

    Allow $OPAMROOT be something else than $HOME/.opam.

--- nfsopam.sh
+++ nfsopam.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-# make opam usable (faster) even if your $HOME is on top of NFS
+# make opam usable (faster) even if your $OPAMROOT is on top of NFS
 # by using /tmp (usually a local disk) for most operations
 
 set -x # debug
 
-# sync ~/.opam to local disk
+# determine the $OPAMROOT
+for i in $(seq $#); do
+    case ${!i} in
+	--root=*)
+	    OPAMROOT=${!i#*=};;
+	--root)
+	    (( i += 1 ))
+	    OPAMROOT=${!i};;
+    esac
+done
+OPAMROOT=${OPAMROOT:-$HOME/.opam}
+
+# sync $OPAMROOT to local disk
 opam clean
 tmp_dot_opam=/tmp/${USER}_dot_opam
-rsync -qa ~/.opam/ $tmp_dot_opam
-mv -f ~/.opam ~/.opam.old
+rsync -qa $OPAMROOT/ $tmp_dot_opam
+mv -f $OPAMROOT $OPAMROOT.old
 
 # use it
-ln -s $tmp_dot_opam ~/.opam
+ln -s $tmp_dot_opam $OPAMROOT
 
 # really call opam
 opam "$@"
 
 # sync back to home on NFS
 opam clean
-\rm ~/.opam
-mv ~/.opam.old ~/.opam
-rsync -qa $tmp_dot_opam/ ~/.opam
+\rm $OPAMROOT
+mv $OPAMROOT.old $OPAMROOT
+rsync -qa $tmp_dot_opam/ $OPAMROOT
